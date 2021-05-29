97b29877e2caf32ef134c174082e73a9eaee2ced
VladislavsGeidans <pisalka30@gmail.com>
2021-05-29 21:49:02 +0300

    Gulp new config

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ODvinksUpdate
 ODvinsk web site design update
+
+```
+    npm install --save-dev
+    npm watch
+```
--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,139 @@
-'use strict';
+"use strict";
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
+// Load plugins
+const browsersync = require("browser-sync").create();
+const del = require("del");
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const newer = require("gulp-newer");
+const plumber = require("gulp-plumber");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+const webpack = require("webpack");
+const webpackconfig = require("./config/webpack.config.js");
+const webpackstream = require("webpack-stream");
+const autoPrefixer = require("gulp-autoprefixer");
 
-var paths = {
-    sass: {
-        src: ['dist/scss/**/*.scss'],
-        dest: 'public/css'
+const env = {
+    server: {
+        path: './',
+        port: 3000,
+        config: {
+            jshint: {
+                src: './config/.jshintrc'
+            }
+        }
+    },
+    paths: {
+        sass: {
+            src: ['./dist/scss/**/*.scss'],
+            dest: './public/css'
+        },
+        html: {
+            src: ['./*.html'],
+            dest: './'
+        },
+        js: {
+            src: ['./dist/js/**/*.js'],
+            dest: './public/js'
+        },
+        images: {
+            src: ['./dist/images/**/*'],
+            dest: './public/images'
+        }
     }
-};
+}
 
-gulp.task('sass', (done) => {
-    gulp.src(paths.sass.src)
-        .pipe(sass())
-        .on('error', sass.logError)
-        .pipe(minifyCss())
-        .pipe(rename({extname: '.min.css'}))
-        .pipe(gulp.dest(paths.sass.dest))
-        .on('end', done);
-});
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: env.server.path,
+    },
+    port: env.server.port
+  });
 
-gulp.task('watch', () => {
-    gulp.watch(paths.sass.src, ['sass', 'reload']);
-});
+  done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+// Clean assets
+function clean() {
+  return del(["./public/"]);
+}
+
+// Optimize Images
+function images() {
+  return gulp
+    .src(env.paths.images.src)
+    .pipe(newer(env.paths.images.dest))
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [
+            {
+              removeViewBox: false,
+              collapseGroups: true
+            }
+          ]
+        })
+      ])
+    )
+    .pipe(gulp.dest(env.paths.images.dest));
+}
+
+// CSS task
+function css() {
+  return gulp
+    .src(env.paths.sass.src)
+    .pipe(plumber())
+    .pipe(sass({ outputStyle: "expanded" }))
+    .pipe(gulp.dest(env.paths.sass.dest))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(autoPrefixer())
+    .pipe(gulp.dest(env.paths.sass.dest))
+    .pipe(browsersync.stream());
+}
+
+// Transpile, concatenate and minify scripts
+function scripts() {
+  return (
+    gulp
+      .src(env.paths.js.src)
+      .pipe(plumber())
+      .pipe(webpackstream(webpackconfig, webpack))
+      // folder only, filename is specified in webpack config
+      .pipe(gulp.dest(env.paths.js.dest))
+      .pipe(browsersync.stream())
+  );
+}
+
+// Watch files
+function watchFiles() {
+  gulp.watch(env.paths.sass.src, css);
+  gulp.watch(env.paths.js.src, gulp.series(scripts));
+  gulp.watch(["./*"], gulp.series(browserSyncReload));
+  gulp.watch(env.paths.images.src, images);
+}
+
+// define complex tasks
+const js = gulp.series(scripts);
+const build = gulp.series(clean, gulp.parallel(css, images, js));
+const watch = gulp.parallel(watchFiles, browserSync);
+
+// export tasks
+exports.images = images;
+exports.css = css;
+exports.js = js;
+exports.clean = clean;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
